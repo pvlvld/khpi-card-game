f84b4e6e38b407c8ed71dc1b562649ca98c2e484
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-05-20 15:07:13 +0300

    test user fetch endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -13,4 +13,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get("user/:username")
+  async getUser(@Param("username") username: string) {
+    const user = await this.userService.findOne({username});
+    return JSON.stringify(user);
+  }
 }
