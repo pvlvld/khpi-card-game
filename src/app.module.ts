c0d455c778e13fa7f753a0b1b64567ef2dc0b50f
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-05-20 11:00:20 +0300

    init additional modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import {Module} from "@nestjs/common";
 import {AppController} from "./app.controller";
 import {AppService} from "./app.service";
+import {AuthModule} from "./auth/auth.module";
+import {UsersModule} from "./users/users.module";
+import {GamesModule} from "./games/games.module";
+import {MatchmakingModule} from "./matchmaking/matchmaking.module";
+import {StatsModule} from "./stats/stats.module";
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UsersModule,
+    GamesModule,
+    MatchmakingModule,
+    StatsModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
