160e385c385ec34555a1c9295b5beff326766d0e
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-05-20 18:51:36 +0300

    jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,17 @@ import {AuthService} from "./auth.service";
 import {AccountModule} from "src/account/account.module";
 import {AccountService} from "src/account/account.service";
 import {PrismaService} from "src/prisma/prisma.service";
+import {JwtModule} from "@nestjs/jwt";
 
 @Module({
-  imports: [AccountModule],
+  imports: [
+    AccountModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: {expiresIn: "30d"}
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService, AccountService, PrismaService]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,25 @@
 import {Injectable, UnauthorizedException} from "@nestjs/common";
+import {JwtService} from "@nestjs/jwt";
 import * as argon2 from "@node-rs/argon2";
 import {AccountService} from "src/account/account.service";
+
 @Injectable()
 export class AuthService {
-  constructor(private accountService: AccountService) {}
+  constructor(
+    private accountService: AccountService,
+    private jwtService: JwtService
+  ) {}
   // TODO: OAuth
   async signIn(username: string, password: string): Promise<any> {
     const account = await this.accountService.findUnique({username});
+
     if (
       !account?.passwordHash ||
       !(await argon2.verify(account.passwordHash, password))
     ) {
       throw new UnauthorizedException();
     }
-    // TODO: JWT
-    return "test";
+
+    return await this.jwtService.signAsync({sub: account.id, username});
   }
 }
