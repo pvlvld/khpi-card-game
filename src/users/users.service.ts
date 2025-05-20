39687531cdf0390c89fafac4223ff3711c1fecc3
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-05-20 14:53:16 +0300

    basic users service methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import {Injectable} from "@nestjs/common";
+import {Prisma, User} from "generated/prisma";
+import {PrismaService} from "src/prisma/prisma.service";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async findOne(where: Prisma.UserWhereUniqueInput): Promise<User | null> {
+    return this.prisma.user.findUnique({where});
+  }
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({data});
+  }
+}
