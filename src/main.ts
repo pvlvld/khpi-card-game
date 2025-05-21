0443f32b29b6b3a6cbd5a057529f2e98d2413ce6
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-05-21 13:24:34 +0300

    cors?

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,20 @@ import {AppModule} from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || origin.startsWith("http://localhost:")) {
+        return callback(null, true);
+      }
+
+      callback(new Error("Not allowed by CORS"), false);
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    allowedHeaders: "Content-Type, Accept, Authorization"
+  });
+
   await app.listen(process.env.NEST_PORT ?? 3069);
 }
 bootstrap();
