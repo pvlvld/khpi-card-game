b538bbbe2b80ec9649f0bbbe82d8ee29cc66a250
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-05-26 21:28:13 +0300

    welcome to costco, i love you!

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -373,7 +373,7 @@ export class GamesService {
     gameState.round++;
     gameState.currentPlayerIndex = 1 - gameState.currentPlayerIndex; // Switch starting player
     gameState.currentPlayerUsername =
-      gameState.players[1 - gameState.currentPlayerIndex].username;
+      gameState.players[gameState.currentPlayerIndex].username;
 
     // Reset round state
     for (const player of gameState.players) {
